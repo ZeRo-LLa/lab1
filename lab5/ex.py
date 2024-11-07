3f9bdea6f05353dd5d634b4753f36186e4dd59cf
ZeRp-LLa <mushyk.illia.s@gmail.com>
2024-11-07 16:06:26 +0200

    newlab5

--- lab5/ex.py
+++ lab5/ex.py
@@ -5,7 +5,6 @@ class Taste(Enum):
     SOUR = "Sour"
     NEUTRAL = "Neutral"
 
-
 class Fruit:
     def __init__(self, name, size, color, taste):
         self.__name = name
@@ -39,14 +38,14 @@ class Fruit:
             self.__taste = taste
         else:
             raise ValueError("Taste must be of type Taste Enum")
-    #repper
+    #repr method
     def __repr__(self):
         return f"Fruit(name='{self.__name}', size={self.__size}, color='{self.__color}', taste={self.__taste})"
     #destructor
     def __del__(self):
         print(f"{self.__name} deleted")
 
-#topping for salad
+#Topping for salad
 class Topping(Enum):
     WHIPPED_CREAM = ("Whipped Cream", Taste.SWEET)
     CHOCOLATE_SYRUP = ("Chocolate Syrup", Taste.SWEET)
@@ -57,7 +56,8 @@ class Topping(Enum):
     def __init__(self, description, taste):
         self.description = description
         self.taste = taste
-#fruct salad
+
+#fruit salad
 class FruitSalad:
     def __init__(self, fruits, topping):
         self.__fruits = fruits 
@@ -84,9 +84,16 @@ class FruitSalad:
     def add_fruit(self, fruit):
         self.__fruits.append(fruit)
 
+    #Function for choosing topping
     def choose_topping_based_on_taste(self):
-        sweet_count = sum(1 for fruit in self.__fruits if fruit.get_taste() == Taste.SWEET)
-        sour_count = sum(1 for fruit in self.__fruits if fruit.get_taste() == Taste.SOUR)
+        sweet_count = 0
+        sour_count = 0
+
+        for fruit in self.__fruits:
+            if fruit.get_taste() == Taste.SWEET:
+                sweet_count += 1
+            elif fruit.get_taste() == Taste.SOUR:
+                sour_count += 1
 
         if sweet_count > sour_count:
             self.__topping = Topping.HONEY
@@ -94,6 +101,7 @@ class FruitSalad:
             self.__topping = Topping.LEMON_JUICE
         else:
             self.__topping = Topping.YOGURT
+
     def __repr__(self):
         fruit_list = ", ".join(repr(fruit) for fruit in self.__fruits)
         return f"FruitSalad(fruits=[{fruit_list}], topping={self.__topping.description})"
@@ -112,3 +120,5 @@ def main():
     salad.choose_topping_based_on_taste()
     print("After choosing topping based on taste:")
     print(salad)
+
+main()
